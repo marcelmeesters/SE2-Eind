a6efb4b2e9c64f59401a61663d023fa6c9789383
Cas Eliëns <cas-eliens@outlook.com>
2015-06-11 15:00:59 +0200

    Add dependencies

---
+++ Pathe/admin/cinemaDelete.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Pathe.admin
+{
+    public partial class cinemaDelete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int bioscoopID = Convert.ToInt32(Request.Form["cinemaID"]);
+            int confirm = Convert.ToInt32(Request.Form["confirm"]);
+
+            if (confirm == 1)
+            {
+                Response.Write(Database.Instance.DeleteCinema(bioscoopID) ? "success" : "fail");
+            }
+            else
+            {
+                Response.Write("noConfirm");
+            }
+        }
+    }
+}
